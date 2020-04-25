95cc7989ce4c7f0dd8ef268b8e3f510593edb25f
meera <meera@softwareassociates.in>
2020-04-25 15:07:05 +0530

    new

--- signUp.js
+++ signUp.js
@@ -5,7 +5,7 @@
 
 let GetUrl = require('./funcs/getURL');
 
-describe("SignUp:", () => {
+describe("SigUp:", () => {
 
     // To be done before each test case
     beforeEach(() => {
@@ -14,7 +14,7 @@ describe("SignUp:", () => {
 
         // Access Signup link
         element(by.css("html > body > app-root > div > app-login > div.lhs-grid > div > a")).click();
-    }); 
+    });
 
     it("Signup with Valid Credentials", function() {
         // Enter all the field values            
@@ -30,7 +30,7 @@ describe("SignUp:", () => {
 
         element(by.css("*[id='billingAddressStreet']")).sendKeys("Street A");
         element(by.css("*[id='billingAddressCity']")).sendKeys("City A");
-        element(by.css("*[id='billingAddressState']")).sendKeys("State A"); 
+        element(by.css("*[id='billingAddressState']")).sendKeys("State A");
 
         element(by.css("*[id='billingAddressZip']")).sendKeys("ABC123");
 
@@ -38,16 +38,16 @@ describe("SignUp:", () => {
         let ele = element(by.css("html > body > app-root > div > app-signup > div.rhs-grid > div > div:nth-of-type(2) > form > div:nth-of-type(2) > div:nth-of-type(7) > label > span"));
         browser.driver.sleep('3000');
 
-        browser.executeScript("arguments[0].click()",ele);
+        browser.executeScript("arguments[0].click()", ele);
 
         let sub = element(by.css("html > body > app-root > div > app-signup > div.rhs-grid > div > div:nth-of-type(2) > form > div:nth-of-type(2) > div:nth-of-type(8) > button"));
         browser.driver.sleep('3000');
-        browser.executeScript("arguments[0].click()",sub);
+        browser.executeScript("arguments[0].click()", sub);
 
         // Validates the success message
         expect(element(by.css("*[id='swal2-title']")).getText()).toBe('User Registration successful!');
-        
-        element(by.css("html > body > div > div > div.swal2-actions > button.swal2-confirm.swal2-styled")).click(); 
+
+        element(by.css("html > body > div > div > div.swal2-actions > button.swal2-confirm.swal2-styled")).click();
 
     });
 
